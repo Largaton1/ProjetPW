e7e87ad269a0bc67a79042b1a31d20ca77d00dd5
K. Largaton <105814979+Largaton1@users.noreply.github.com>
2023-12-30 00:58:56 +0100

    mise en place de la connexion pour admin

---
+++ auth/login.php
@@ -0,0 +1,22 @@
+<?php
+   
+    session_start();
+
+    if ($_POST['email'] == 'test@example.com' && $_POST['password'] == 'mdp') {
+      
+        $_SESSION['user'] = [
+            'id' => 1,
+            'nom' => 'Cyril',
+            'prenom' => 'KONE',
+            'email' => 'largaton95@gmail.com',
+            'est_administrateur' => true
+        ];
+
+        header('Location: index.php'); // Redirection vers la page dédiée à l'administrateur
+        exit();
+    } else {
+        // Informations incorrectes, rediriger vers la page de connexion avec un message d'erreur
+        header('Location: login.php?error=1');
+        exit();
+    }
+?>
