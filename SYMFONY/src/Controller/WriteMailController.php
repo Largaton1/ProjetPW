16f7a45055c43446207382edd5fc5bb0d9c8efc0
K. Largaton <105814979+Largaton1@users.noreply.github.com>
2024-01-17 16:21:30 +0100

    fix writeMailController

--- SYMFONY/src/Controller/WriteMailController.php
+++ SYMFONY/src/Controller/WriteMailController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategorieRepository;
+use App\Entity\Educateur;
 use App\Repository\EducateurRepository;
 use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,15 +14,15 @@ use Symfony\Component\HttpFoundation\Request;
 class WriteMailController extends AbstractController
 {
     #[Route('/write/mail/{entite}', name: 'app_write_mail')]
-    public function index(EducateurRepository $educateurRepository, CategorieRepository $categorieRepository, Request $request): Response
+    public function index(EducateurRepository $educateurRepository, ContactRepository $contactRepository, Request $request): Response
     {
         $entite = $request->get('entite');
         $user = $this->getUser();
         
-       $ideduc=$user->getId();
+       $recupIdEdu=$user->getUserIdentifier();
        if($entite=='educateur')
        {
-        $educateurs = $educateurRepository->findAllEducateurOne($ideduc);
+        $educateurs = $educateurRepository->findAllEducateurOne($recupIdEdu);
  
        }
        else{
